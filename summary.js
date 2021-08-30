c42b4403809377a19b0b97b0f9ebaaa7e0b80803
Faisal-Khan-Tushar <faisal15-13400@diu.edu.bd>
2021-08-30 18:23:45 +0600

    summary done

---
+++ summary.js
@@ -0,0 +1,31 @@
+let a = 13;
+let b = 'Tushar'
+let c = false;
+//primitive,non primitive
+/* 
+pirmitive-
+1.number
+2.string
+3.boolean
+4.undefined
+5.null
+6.symbol
+non-primitive
+object */
+let array = [1, 2, 3, 4]
+// console.log(typeof (array))
+function triple(x, y, z) {
+  x.age = 22;
+  // x = 111;
+  // y = 222;
+  // z = 333;
+}
+const num1 = 10;
+const num2 = 20;
+const num3 = 30;
+const myObj = { name: 'Faisal Khan Tushar', age: 55 }
+triple(myObj);
+console.log(myObj);
+triple(num1, num2, num3);
+// console.log(triple.length)
+console.log(num1, num2, num3)
