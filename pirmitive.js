d14cc7fb213053a1d228cd99eb57a644b29876ad
Faisal-Khan-Tushar <faisal15-13400@diu.edu.bd>
2021-08-30 13:26:52 +0600

    second push

---
+++ pirmitive.js
@@ -0,0 +1,30 @@
+/*
+Data types
+1.number
+2.string
+3.boolean
+4.undefined
+5.null
+6.object
+7.symbol
+primitive-
+number,string,boolean,undefined, null & symbol
+non primiive -
+object
+
+*/
+//primitive -> has one value or one primary value. They are immutable
+let a = 10;
+let b = a;
+// console.log(a, b)
+a = 20;
+// console.log(a, b)
+
+//non pirmitive-> objects
+let x = { job: 'website developer' }
+let y = x;
+console.log(x, y)
+x.job = 'font end developer'
+// console.log(x, y)
+y.job = 'back end developer'
+console.log(x, y);
