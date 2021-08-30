7740d66dd53cf40695c8f8b470fe54956e8ae49f
Faisal-Khan-Tushar <faisal15-13400@diu.edu.bd>
2021-08-30 16:02:20 +0600

    fifth push

---
+++ double-triple.js
@@ -0,0 +1,19 @@
+const first = '0';
+const second = false;
+if (first === second) {
+  // console.log('yes both are same')
+}
+else {
+  // console.log('no both are not same');
+}
+//more comparisions
+// const person1 = { name: 'Tushar' }
+// const person2 = { name: 'Tushar' }
+const array1 = [];
+const array2 = [];
+if (array1 === array2) {
+  console.log('yes both person are same')
+}
+else {
+  console.log('no both person are not same')
+}
