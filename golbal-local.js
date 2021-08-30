5eb68dfc2db99a50ba3b883f308fffa88786dc8f
Faisal-Khan-Tushar <faisal15-13400@diu.edu.bd>
2021-08-30 16:35:12 +0600

    sixth commit

---
+++ golbal-local.js
@@ -0,0 +1,27 @@
+
+const favouriteNumber = 10;
+function add(first, second) {
+
+  // console.log(mood);
+  const result = first + second;
+  // console.log(favouriteNumber)
+  if (result > 10) {
+    mood = 'happy'
+    mood = 'kharap';
+    // console.log(mood)
+
+  }
+  // console.log(mood)
+
+
+  return result;
+}
+const res = add(10, 20);
+
+// console.log(favouriteNumber)
+// console.log(mood)
+
+for (let i = 0; i < 10; i++) {
+
+}
+console.log(i);
